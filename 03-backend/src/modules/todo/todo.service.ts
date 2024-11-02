844ffd3f2ad6ee40ab7c4e17cae548d85a87fe46
WJurgiel <wjurgiel@student.agh.edu.pl>
2024-11-02 21:14:16 +0100

    nest backend added

---
+++ 03-backend/src/modules/todo/todo.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+
+@Injectable()
+export class TodoService {
+  listTodo() {
+    return [
+      {
+        title: 'TEST',
+        content: 'TEST',
+        done: true,
+      },
+    ];
+  }
+  addTodo(data: CreateTodoDto) {
+    return data;
+  }
+  editTodo() {}
+  deleteTodo() {}
+}
