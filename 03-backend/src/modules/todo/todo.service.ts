aa97679c693c79a577632df5e5c25d77b3b27a9e
WJurgiel <wjurgiel@student.agh.edu.pl>
2024-11-03 17:19:55 +0100

    database added

--- 03-backend/src/modules/todo/todo.service.ts
+++ 03-backend/src/modules/todo/todo.service.ts
@@ -1,20 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { EditTodoDto } from './dto/edit-todo.dto';
+import { TodoFilterDto } from './dto/todo-filter.dto';
 
 @Injectable()
 export class TodoService {
-  listTodo() {
-    return [
-      {
-        title: 'TEST',
-        content: 'TEST',
-        done: true,
+  constructor(private readonly prisma: PrismaService) {}
+
+  async listTodo(filter: TodoFilterDto) {
+    return this.prisma.todo.findMany({
+      where: {
+        done: filter.isDone,
       },
-    ];
+      orderBy: {
+        [filter.sortBy]: filter.sortOrder,
+      },
+    });
   }
   addTodo(data: CreateTodoDto) {
-    return data;
+    return this.prisma.todo.create({
+      data: {
+        title: data.title,
+        content: data.content,
+        done: data.done,
+      },
+    });
   }
-  editTodo() {}
-  deleteTodo() {}
+  editTodo(id: number, data: EditTodoDto) {
+    return this.prisma.todo.update({
+      where: { id },
+      data,
+    });
+  }
+  deleteTodo(id: number) {
+    return this.prisma.todo.delete({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  get(id: number) {
+    return this.prisma.todo.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
 }
